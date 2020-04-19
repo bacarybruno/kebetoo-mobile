462f8d750692f63294d4954e4aad4a2b4a23b715
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2020-04-19 11:11:41 +0200

    style(navigation): adjust bottom tab bar style

--- src/shared/components/buttons/tab-bar/styles.js
+++ src/shared/components/buttons/tab-bar/styles.js
@@ -1,31 +1,31 @@
 import { StyleSheet } from 'react-native'
 
 import colors from 'Kebetoo/src/theme/colors'
-import metrics from 'Kebetoo/src/theme/metrics'
 
-const size = 71
-const padding = 15
+const size = 56
 export default StyleSheet.create({
   wrapper: {
     width: size,
     height: size,
+    marginBottom: size / 2,
   },
   container: {
-    width: size,
-    height: size,
+    width: '100%',
+    height: '100%',
     borderRadius: size / 2,
+    backgroundColor: colors.primary,
     position: 'absolute',
     bottom: size / 2,
     justifyContent: 'center',
     alignItems: 'center',
-    padding,
-  },
-  content: {
-    width: size - padding,
-    height: size - padding,
-    borderRadius: size - padding / 2,
-    backgroundColor: colors.primary,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // shadow / elevation
+    shadowColor: colors.black,
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+    elevation: 6,
   },
 })
