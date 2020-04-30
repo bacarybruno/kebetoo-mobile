a9ab6a3dfad976970ecef753142f2b627fa55340
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2020-04-30 15:03:51 +0200

    feat: add posts and comments placeholder

---
+++ src/shared/components/placeholders/posts.js
@@ -0,0 +1,47 @@
+import React, { memo } from 'react'
+import { View } from 'react-native'
+import {
+  Placeholder, PlaceholderMedia, PlaceholderLine, Fade,
+} from 'rn-placeholder'
+
+const PlaceholderAvatar = () => (
+  <PlaceholderMedia isRound style={{ marginRight: 10 }} />
+)
+
+const PlaceholderHeader = () => (
+  <Placeholder Animation={Fade} Left={PlaceholderAvatar} style={{ marginBottom: 8 }}>
+    <PlaceholderLine height={10} width={50} />
+    <PlaceholderLine height={10} width={30} />
+  </Placeholder>
+)
+
+const PlaceholderContent = () => (
+  <Placeholder Animation={Fade}>
+    <PlaceholderLine height={52} style={{ marginBottom: 8, borderRadius: 5 }} />
+  </Placeholder>
+)
+
+const PlaceholderReaction = () => (
+  <PlaceholderLine width={15} height={20} style={{ marginRight: 10 }} />
+)
+
+const PlaceholderReactions = () => (
+  <Placeholder style={{ marginBottom: 30 }}>
+    <View style={{ flexDirection: 'row' }}>
+      <PlaceholderReaction />
+      <PlaceholderReaction />
+      <PlaceholderReaction />
+      <PlaceholderReaction />
+    </View>
+  </Placeholder>
+)
+
+const PostPlaceholder = () => (
+  <>
+    <PlaceholderHeader />
+    <PlaceholderContent />
+    <PlaceholderReactions />
+  </>
+)
+
+export default memo(PostPlaceholder)
