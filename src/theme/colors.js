158fa78c98512b9861944b46fb2f259f8165a6b8
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2020-05-15 19:59:00 +0200

    refactor: update hex to rgb function to remove eslint warning

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -18,8 +18,8 @@ const colors = {
 }
 
 export const hexToRgb = (hex) => {
-  hex = hex.replace('#', '')
-  const bigint = parseInt(hex, 16)
+  const sanitizedHex = hex.replace('#', '')
+  const bigint = parseInt(sanitizedHex, 16)
   const r = (bigint >> 16) & 255
   const g = (bigint >> 8) & 255
   const b = bigint & 255
