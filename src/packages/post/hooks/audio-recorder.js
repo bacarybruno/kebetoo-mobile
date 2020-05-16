9f4648193773a29460736ce040f773284dd26f23
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2020-05-16 13:09:02 +0200

    feat: add rn-fetch-blob and basic file upload

--- src/packages/post/hooks/audio-recorder.js
+++ src/packages/post/hooks/audio-recorder.js
@@ -2,11 +2,18 @@ import {
   useState, useEffect, useCallback, useRef,
 } from 'react'
 import { Recorder, Player } from '@react-native-community/audio-toolkit'
+import RNFetchBlob from 'rn-fetch-blob'
+import * as api from 'Kebetoo/src/shared/helpers/http'
 
 import { usePermissions } from 'Kebetoo/src/shared/hooks'
 
 export const MAX_DURATION_IN_SECONDS = 30
 export const RECORD_NAME = 'kebetoo-record.aac'
+export const RECORD_MIME_TYPE = 'audio/aac'
+
+export const getUri = (filename = RECORD_NAME) => (
+  `${RNFetchBlob.fs.dirs.DocumentDir}/${filename}`
+)
 
 const useAudioRecorder = (maxDuration) => {
   const maxDurationInSeconds = maxDuration || MAX_DURATION_IN_SECONDS
@@ -19,29 +26,49 @@ const useAudioRecorder = (maxDuration) => {
   const start = useCallback(async () => {
     const hasPermissions = await permissions.recordAudio()
     if (hasPermissions) {
-      setRecorder(new Recorder(RECORD_NAME).record())
+      setElapsedTime(0)
+      setRecorder(new Recorder(RECORD_NAME, {
+        bitrate: 32000,
+        sampleRate: 32000,
+        channels: 1,
+        quality: 'min',
+      }).record())
       setIsRecording(true)
     }
   }, [permissions])
 
-  const stop = useCallback(() => {
-    recorder.stop()
-    new Player(RECORD_NAME).play()
+  const stop = useCallback(async () => {
     setIsRecording(false)
+    if (recorder) {
+      recorder.stop()
+      new Player(RECORD_NAME).play()
+      const response = await api.createPostWithAudio({
+        author: 'RUCPMUzpttYRz43Qw29Xeg0Lk592',
+        content: 'Hello first audio from code',
+        audio: {
+          uri: getUri(),
+          type: RECORD_MIME_TYPE,
+          name: 'hello-world',
+        },
+      })
+      console.log('create post with audio success', response)
+      await RNFetchBlob.fs.unlink(getUri())
+    }
   }, [recorder])
 
   useEffect(() => {
-    if (isRecording) {
+    if (isRecording && !intervalRef.current) {
+      const timeStart = Date.now()
       intervalRef.current = setInterval(() => {
-        setElapsedTime((value) => value + 1)
-      }, 1000)
-    } else {
-      clearTimeout(intervalRef.current)
-      setElapsedTime(0)
+        const delta = Date.now() - timeStart
+        setElapsedTime(Math.floor(delta / 1000))
+      }, 100)
+    }
+    return () => {
+      clearInterval(intervalRef.current)
       intervalRef.current = null
     }
-    return () => clearInterval(intervalRef.current)
-  }, [isRecording])
+  }, [isRecording, intervalRef])
 
   useEffect(() => {
     if (elapsedTime === maxDurationInSeconds) {
