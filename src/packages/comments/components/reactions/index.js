3017dd7fe71168904a05b5eb8bbed2a609649d7c
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2020-05-17 20:54:05 +0200

    fix(comments): update reactions to use offline or online component

--- src/packages/comments/components/reactions/index.js
+++ src/packages/comments/components/reactions/index.js
@@ -1,19 +1,26 @@
 import React, { memo } from 'react'
 import { View } from 'react-native'
+import { useSelector } from 'react-redux'
 
+import ReactionsOffline from 'Kebetoo/src/packages/post/containers/reactions'
 import ReactionsOnline from 'Kebetoo/src/packages/post/containers/reactions/online'
+import { postsSelector } from 'Kebetoo/src/redux/selectors'
 
 import DraggableIndicator from '../draggable-indicator'
 
 import styles from './styles'
 
-const Reactions = ({ post, author, ...reactionProps }) => (
-  <View style={styles.reactionsContainer}>
-    <DraggableIndicator />
-    <View style={styles.reactions}>
-      <ReactionsOnline post={post} author={author} {...reactionProps} />
+const Reactions = ({ post, author, ...reactionProps }) => {
+  const posts = useSelector(postsSelector)
+  const ReactionsComponent = posts[post.id] ? ReactionsOffline : ReactionsOnline
+  return (
+    <View style={styles.reactionsContainer}>
+      <DraggableIndicator />
+      <View style={styles.reactions}>
+        <ReactionsComponent post={post} author={author} {...reactionProps} />
+      </View>
     </View>
-  </View>
-)
+  )
+}
 
 export default memo(Reactions)
