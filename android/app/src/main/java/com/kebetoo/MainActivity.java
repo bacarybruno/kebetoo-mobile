3e2cb6a3932079b1908105be5ddc0fe05081c316
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2020-06-13 20:55:34 +0200

    fix: incoming sharing capability reuse the same task on android

--- android/app/src/main/java/com/kebetoo/MainActivity.java
+++ android/app/src/main/java/com/kebetoo/MainActivity.java
@@ -1,15 +1,21 @@
 package com.kebetoo;
 
 import android.os.Bundle;
-
 import com.facebook.react.ReactActivity;
 import com.zoontek.rnbootsplash.RNBootSplash;
+import android.content.Intent;
 
 public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     RNBootSplash.init(R.drawable.bootsplash, MainActivity.this);
+  }
+
+  @Override
+  public void onNewIntent(Intent intent) {
+    super.onNewIntent(intent);
+    setIntent(intent);
   }
 
   /**
