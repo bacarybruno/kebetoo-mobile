1b1d0bf796e47842103ddbb28a040368747e3c1e
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2020-06-23 19:00:09 +0200

    chore: run test on commit

--- src/packages/onboarding/components/__tests__/index.test.js
+++ src/packages/onboarding/components/__tests__/index.test.js
@@ -8,7 +8,9 @@ it('renders Onboarding', () => {
   const { wrapper } = givenOnboarding({
     slideTitle: 'Kebetoo is gonna change the way your communicate',
     slideDescription: 'Just wait for it!',
-    imageSrc: 'https://avatars1.githubusercontent.com/u/14147533',
+    imageSrc: {
+      uri: 'https://avatars1.githubusercontent.com/u/14147533',
+    },
   })
   expect(wrapper.toJSON()).toMatchSnapshot()
 })
