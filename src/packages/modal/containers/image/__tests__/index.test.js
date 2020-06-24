27609e02b0d8d7fb5294e1ad14ab917b6ea76a41
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2020-06-24 15:11:43 +0200

    test: add test for imagemodal

---
+++ src/packages/modal/containers/image/__tests__/index.test.js
@@ -0,0 +1,25 @@
+import setupTest from 'Kebetoo/src/config/jest-setup'
+
+import ImageModal from '../index'
+
+jest.mock('@react-navigation/native', () => ({
+  ...jest.requireActual('@react-navigation/native'),
+  useRoute: () => ({
+    params: {
+      url: '/fake-image.png',
+      width: 100,
+      height: 100,
+    },
+  }),
+}))
+
+const givenImageModal = setupTest(ImageModal)({
+  navigation: {
+    setOptions: jest.fn(),
+  },
+})
+
+it('renders ImageModal', () => {
+  const { wrapper } = givenImageModal()
+  expect(wrapper.toJSON()).toMatchSnapshot()
+})
--- src/packages/modal/containers/image/index.js
+++ src/packages/modal/containers/image/index.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Image, StatusBar } from 'react-native'
 import { useRoute } from '@react-navigation/native'
 import { TransitionPresets } from '@react-navigation/stack'
+
 import { getSource } from 'Kebetoo/src/packages/post/components/image-content'
 import HeaderBack from 'Kebetoo/src/shared/components/header-back'
 import colors from 'Kebetoo/src/theme/colors'
@@ -23,12 +24,26 @@ const ImageModal = ({ navigation }) => {
 
   const { params } = useRoute()
   const { url, width, height } = params
+  const source = getSource(url)
+  const [aspectRatio, setAspectRatio] = useState(parseInt(width, 10) / parseInt(height, 10))
+
+  useEffect(() => {
+    if (Number.isNaN(aspectRatio)) {
+      Image.getSize(source, (w, h) => {
+        // set aspect ratio based on react native image size
+        setAspectRatio(w / h)
+      }, () => {
+        // on error
+        setAspectRatio(0)
+      })
+    }
+  }, [aspectRatio, source])
 
   return (
     <>
       <StatusBar barStyle="light-content" />
       <View style={styles.wrapper}>
-        <Image source={getSource(url)} style={{ aspectRatio: width / height }} />
+        <Image source={source} style={{ aspectRatio }} />
       </View>
     </>
   )
