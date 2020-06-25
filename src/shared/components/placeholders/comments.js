ae86fea2e512af970c69b2e9f4d7dc039162198b
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2020-06-25 21:03:14 +0200

    style: change comment placeholder style

--- src/shared/components/placeholders/comments.js
+++ src/shared/components/placeholders/comments.js
@@ -1,18 +1,38 @@
 import React from 'react'
+import { StyleSheet } from 'react-native'
 import {
   Placeholder, PlaceholderMedia, PlaceholderLine, Fade,
 } from 'rn-placeholder'
 
+const styles = StyleSheet.create({
+  placeholderIcon: {
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+  },
+  placeholderAvatar: {
+    marginRight: 10,
+  },
+  placeholder: {
+    height: 60,
+  },
+})
+
 const PlaceholderIcon = () => (
-  <PlaceholderMedia size={15} style={{ marginLeft: 10 }} />
+  <PlaceholderMedia size={15} style={styles.placeholderIcon} />
 )
 
 const PlaceholderAvatar = () => (
-  <PlaceholderMedia isRound style={{ marginRight: 10 }} />
+  <PlaceholderMedia isRound style={styles.placeholderAvatar} />
 )
 
 const CommentPlaceholder = () => (
-  <Placeholder Animation={Fade} Left={PlaceholderAvatar} Right={PlaceholderIcon}>
+  <Placeholder
+    style={styles.placeholder}
+    Animation={Fade}
+    Left={PlaceholderAvatar}
+    Right={PlaceholderIcon}
+  >
     <PlaceholderLine height={8} width={50} />
     <PlaceholderLine height={10} style={{ marginBottom: 5 }} />
     <PlaceholderLine height={10} />
