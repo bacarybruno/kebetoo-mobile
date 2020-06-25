2dc84301f303696ca458c41fcc308537be84c6f7
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2020-06-25 21:03:14 +0200

    style: change comment placeholder style

--- src/shared/components/no-content/__tests__/index.test.js
+++ src/shared/components/no-content/__tests__/index.test.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import setupTest from 'Kebetoo/src/config/jest-setup'
+import strings from 'Kebetoo/src/config/strings'
 
 import NoContent from '../index'
 import Text from '../../text'
 
 const givenNoContent = setupTest(NoContent)({
-  title: 'Hello World!',
-  text: 'Nothing here. Please leave!',
+  title: strings.general.no_content,
+  text: strings.comments.no_content,
   children: <Text text="child" />,
 })
 
