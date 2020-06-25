93f16498ac2b3cf02dd31ed6a0bed130210cd5a7
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2020-06-25 10:56:41 +0200

    test: add tests for post component [WIP]

---
+++ src/packages/post/components/audio-content/__tests__/index.test.js
@@ -0,0 +1,26 @@
+import { render, fireEvent } from 'react-native-testing-library'
+
+import { constructFileName } from 'Kebetoo/src/shared/hooks/audio-recorder'
+import setupTest from 'Kebetoo/src/config/jest-setup'
+
+import AudioContent from '../index'
+
+const givenAudioContent = setupTest(AudioContent, render)({
+  content: 'test audio',
+  audioUrl: '/fake-audio.mp3',
+  style: { elevation: 3 },
+  onPress: jest.fn(),
+  // date format: YYYYMMDD
+  audioName: constructFileName(20200625, 10),
+})
+
+it('renders AudioContent', () => {
+  const { wrapper } = givenAudioContent()
+  expect(wrapper.toJSON()).toMatchSnapshot()
+})
+
+it('handles player press', () => {
+  const { wrapper, props } = givenAudioContent()
+  fireEvent.press(wrapper.getByTestId('player-wrapper'))
+  expect(props.onPress).toBeCalledTimes(1)
+})
---
+++ src/packages/post/components/icon-button/__tests__/index.test.js
@@ -0,0 +1,31 @@
+import setupTest from 'Kebetoo/src/config/jest-setup'
+import { readableSeconds } from 'Kebetoo/src/shared/helpers/dates'
+import { act } from 'react-test-renderer'
+
+import IconButton from '../index'
+
+const givenIconButton = setupTest(IconButton)({
+  onPress: jest.fn(),
+  name: 'microphone',
+  style: {
+    elevation: 5,
+  },
+})
+
+it('renders IconButton', () => {
+  const { wrapper } = givenIconButton()
+  expect(wrapper.toJSON()).toMatchSnapshot()
+})
+
+it('renders activable IconButton', () => {
+  let wrapper = null
+  act(() => {
+    const { wrapper: asyncWrapper } = givenIconButton({
+      activable: true,
+      isActive: true,
+      text: readableSeconds(100),
+    })
+    wrapper = asyncWrapper
+  })
+  expect(wrapper.toJSON()).toMatchSnapshot()
+})
