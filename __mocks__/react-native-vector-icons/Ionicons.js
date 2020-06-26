b16372b1584fbe4339b75006732c67fa11521bd5
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2020-06-26 20:51:17 +0200

    test: mock ionicons so that snapshot are more explicit

---
+++ __mocks__/react-native-vector-icons/Ionicons.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import Text from 'Kebetoo/src/shared/components/text'
+import colors from 'Kebetoo/src/theme/colors'
+
+// custom icons doesn't show up correctly
+// so, create a custom mock
+export default ({
+  name, size, color, style, ...otherProps
+}) => (
+  <Text
+    allowFontScaling={false}
+    text={`ionicon-${name}`}
+    fontSize={size}
+    color={Object.keys(colors).find((c) => colors[c] === color)}
+    style={{
+      color,
+      fontFamily: 'Ionicons',
+      fontStyle: 'normal',
+      fontWeight: 'normal',
+      ...style,
+    }}
+    {...otherProps}
+  />
+)
