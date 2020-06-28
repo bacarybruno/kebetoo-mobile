41e354ed2a15ddd0b09bae85e06a1182ca373c3c
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2020-06-28 02:02:57 +0200

    test: add snapshot test for post creation page

---
+++ src/packages/post/containers/create/__tests__/index.test.js
@@ -0,0 +1,15 @@
+import setupTest from 'Kebetoo/src/config/jest-setup'
+
+import CreatePost from '../index'
+
+const givenCreatePost = setupTest(CreatePost)({
+  navigation: {
+    navigate: jest.fn(),
+    setOptions: jest.fn(),
+  },
+})
+
+it('renders CreatePost', () => {
+  const { wrapper } = givenCreatePost()
+  expect(wrapper.toJSON()).toMatchSnapshot()
+})
