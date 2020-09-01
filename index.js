47daad7d125fa47f8512ebb4f5c6cdabcb432db3
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2020-09-02 00:11:40 +0200

    feat(notifications): set badge number on background notification

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import { AppRegistry } from 'react-native'
+import PushNotification from 'react-native-push-notification'
+import AsyncStorage from '@react-native-community/async-storage'
+import messaging from '@react-native-firebase/messaging'
 
 import { name as appName } from './app.json'
 import App from './src'
+
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  const bgNotifications = JSON.parse(await AsyncStorage.getItem('backgroundNotifications')) || []
+  bgNotifications.push(remoteMessage)
+  PushNotification.setApplicationIconBadgeNumber(bgNotifications.length)
+  await AsyncStorage.setItem('backgroundNotifications', JSON.stringify(bgNotifications))
+})
 
 AppRegistry.registerComponent(appName, () => App)
