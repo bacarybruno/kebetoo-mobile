655601c3ec6eb92ad4751748bfd427ceef28b1ee
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2020-09-03 00:33:30 +0200

    feat(notifications): create hook and persist messages to redux

--- src/shared/hooks/index.js
+++ src/shared/hooks/index.js
@@ -1,9 +1,11 @@
 import useKeyboard from './keyboard'
 import usePermissions from './permissions'
+import useNotifications from './notifications'
 import useAudioRecorder from './audio-recorder'
 
 export {
   useKeyboard,
   usePermissions,
+  useNotifications,
   useAudioRecorder,
 }
