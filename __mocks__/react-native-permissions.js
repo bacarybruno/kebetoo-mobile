8d81930387f28e862248b7c3cb799505f971a7c0
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2020-09-29 15:32:54 +0200

    fix: audio not working when stored online

--- __mocks__/react-native-permissions.js
+++ __mocks__/react-native-permissions.js
@@ -3,3 +3,4 @@ const { PERMISSIONS, RESULTS } = require('react-native-permissions/lib/commonjs/
 export { PERMISSIONS, RESULTS }
 
 export const request = jest.fn().mockResolvedValue(RESULTS.GRANTED)
+export const check = jest.fn().mockResolvedValue(RESULTS.GRANTED)
