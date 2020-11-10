5c9e0f8420191f85378190a177347bcfaf55afd6
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2020-11-10 10:37:13 +0100

    test: add correct mock for rnlocalization

--- __mocks__/react-native-localization.js
+++ __mocks__/react-native-localization.js
@@ -1,37 +1,10 @@
-export default class RNLocalization {
-  language = 'en'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import LocalizedStringsCore from 'react-localization'
 
+const getInterfaceLanguage = () => 'en'
+
+export default class LocalizedStrings extends LocalizedStringsCore {
   constructor(props) {
-    this.props = props
-    this.setLanguage(this.language)
-  }
-
-  setLanguage(interfaceLanguage) {
-    this.language = interfaceLanguage
-    if (this.props[interfaceLanguage]) {
-      const localizedStrings = this.props[this.language]
-      Object.keys(localizedStrings).forEach((key) => {
-        if (localizedStrings.hasOwnProperty(key)) {
-          this[key] = localizedStrings[key]
-        }
-      })
-    }
-  }
-
-  setInterfaceLanguage(interfaceLanguage) {
-    this.setLanguage(interfaceLanguage)
-  }
-
-  getLanguage() {
-    return this.language
-  }
-
-  getInterfaceLanguage() {
-    return this.language
-  }
-
-  formatString(key) {
-    const localizedStrings = this.props[this.language]
-    return localizedStrings[key]
+    super(props, getInterfaceLanguage)
   }
 }
