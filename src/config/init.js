5ceb9cfcf69f45b801523302db1d714870bada73
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2020-11-29 19:30:13 +0100

    feat: use a dotenv plugin

--- src/config/init.js
+++ src/config/init.js
@@ -1,6 +1,7 @@
 import { GoogleSignin } from '@react-native-community/google-signin'
+import env from './env'
 
 GoogleSignin.configure({
   scopes: ['email', 'profile', 'openid'],
-  webClientId: '355888072499-spu5d874qrfb6tfs3pq195cjf6ij56go.apps.googleusercontent.com',
+  webClientId: env.googleSigninWebClientId,
 })
