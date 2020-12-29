417c47f73dba58b3b1a1cc9f1d2864f6e1412adc
Bruno Bodian <bacarybruno@gmail.con>
2020-12-29 10:54:42 +0000

    style(ios): use the right top and bottom safearea colors in iPhoneX

--- src/features/app/containers/styles.js
+++ src/features/app/containers/styles.js
@@ -1,7 +1,19 @@
 import { StyleSheet } from 'react-native'
+import { ifIphoneX } from 'react-native-iphone-x-helper'
 
-export default StyleSheet.create({
+export default (colors) => StyleSheet.create({
   wrapper: {
     flex: 1,
+    backgroundColor: colors.background,
+  },
+  bottomSafeArea: {
+    flex: 0,
+    backgroundColor: colors.colorScheme === 'dark'
+      ? ifIphoneX(colors.backgroundSecondary, colors.background)
+      : colors.background,
+  },
+  topSafeArea: {
+    flex: 0,
+    backgroundColor: colors.background,
   },
 })
