7f3f78c817220a9b8753f044681c0e2d0ede7b4a
Bruno Bodian <bacarybruno@gmail.con>
2021-01-04 08:00:46 +0000

    chore(ios): build the app on release ios

--- ios/kebetoo_pods.rb
+++ ios/kebetoo_pods.rb
@@ -13,4 +13,19 @@ def react_native_share_menu_post_install(installer)
           config.build_settings['APPLICATION_EXTENSION_API_ONLY'] = 'NO'
         end
     end
+    # ref https://medium.com/@khushwanttanwar/xcode-12-compilation-errors-while-running-with-ios-14-simulators-5731c91326e9
+    installer.pods_project.build_configurations.each do |config|
+        config.build_settings["EXCLUDED_ARCHS[sdk=iphonesimulator*]"] = "arm64"
+    end
+end
+
+# Post Install processing for Flipper
+def flipper_post_install(installer)
+  installer.pods_project.targets.each do |target|
+    if target.name == 'YogaKit'
+      target.build_configurations.each do |config|
+        config.build_settings['SWIFT_VERSION'] = '4.1'
+      end
+    end
+  end
 end
