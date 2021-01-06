686d7c04919b8695a42c7c8b6dc9bdefbfe19e78
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2021-01-06 11:39:51 +0100

    test(post): unit test catchup

---
+++ src/features/post/containers/create/__tests__/index.audio.test.js
@@ -0,0 +1,34 @@
+import setupTest from '@app/config/jest-setup'
+import { AudioPlayer } from '@app/shared/components/audio-player'
+
+import CreatePost, { actionTypes } from '../index'
+
+jest.mock('@app/shared/hooks', () => ({
+  useAudioRecorder: jest.fn().mockReturnValue({ hasRecording: true }),
+  useAnalytics: jest.fn().mockReturnValue({ reportError: jest.fn() }),
+  useAppColors: jest.fn().mockReturnValue({ colors: {} }),
+  useAppStyles: jest.fn().mockReturnValue({ icon: {} }),
+  useKeyboard: jest.fn().mockReturnValue({}),
+  useUser: jest.fn().mockReturnValue({ profile: {} }),
+}))
+
+const givenCreatePost = setupTest(CreatePost)({
+  navigation: {
+    navigate: jest.fn(),
+    setOptions: jest.fn().mockImplementation((params) => {
+      if (params.headerRight) params.headerRight()
+    }),
+    dispatch: jest.fn(),
+  },
+  route: {
+    params: {
+      action: actionTypes.SHARE,
+    },
+  },
+})
+
+it('renders AudioPlayer if post has recording', () => {
+  const { wrapper } = givenCreatePost()
+  expect(wrapper.toJSON()).toMatchSnapshot()
+  expect(wrapper.root.findAllByType(AudioPlayer).length).toBe(1)
+})
