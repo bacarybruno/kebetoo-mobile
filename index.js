dc200f370416ede25503d4fe9ee3de53eae05f64
BODIAN Bacary Bruno <bacarybruno@gmail.com>
2021-01-08 01:20:54 +0100

    fix: disable analytics collection is device is running in test lab

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import PushNotification from 'react-native-push-notification'
 import AsyncStorage from '@react-native-community/async-storage'
 import messaging from '@react-native-firebase/messaging'
 import { KeyboardRegistry } from 'react-native-ui-lib/keyboard'
+import { utils } from '@react-native-firebase/app'
+import analytics from '@react-native-firebase/analytics'
 
 import { EmojiSelector } from '@app/shared/components'
 import { keyboardName } from '@app/shared/components/emoji-selector'
@@ -10,12 +12,19 @@ import { keyboardName } from '@app/shared/components/emoji-selector'
 import { name as appName } from './app.json'
 import App from './src'
 
+// handle background notifications
 messaging().setBackgroundMessageHandler(async (remoteMessage) => {
   const bgNotifications = JSON.parse(await AsyncStorage.getItem('backgroundNotifications')) || []
   bgNotifications.push(remoteMessage)
   PushNotification.setApplicationIconBadgeNumber(bgNotifications.length)
   await AsyncStorage.setItem('backgroundNotifications', JSON.stringify(bgNotifications))
 })
+
+if (utils().isRunningInTestLab) {
+  // do not track analytics for firebase test lab
+  // used by google play to run test on publish
+  analytics().setAnalyticsCollectionEnabled(false)
+}
 
 AppRegistry.registerComponent(appName, () => App)
 KeyboardRegistry.registerKeyboard(keyboardName, () => EmojiSelector)
