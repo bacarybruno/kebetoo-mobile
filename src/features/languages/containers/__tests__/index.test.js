6dc932a966d2265f35a53fa1f59752e5b42e4ded
Bruno Bodian <bacarybruno@gmail.con>
2021-02-27 00:53:56 +0100

    test(languagues): add missing test

--- src/features/languages/containers/__tests__/index.test.js
+++ src/features/languages/containers/__tests__/index.test.js
@@ -1,3 +1,6 @@
+import { Alert } from 'react-native'
+
+import { strings } from '@app/config'
 import setupTest from '@app/config/jest-setup'
 
 import LanguagesPage from '../index'
@@ -9,4 +12,13 @@ const givenLanguagesPage = setupTest(LanguagesPage)()
 it('renders LanguagesPage', () => {
   const { wrapper } = givenLanguagesPage()
   expect(wrapper.toJSON()).toMatchSnapshot()
+})
+
+it('updates language', () => {
+  Alert.alert = jest.fn()
+  const { wrapper } = givenLanguagesPage()
+  const saveButton = wrapper.root.findByProps({ text: strings.general.save.toUpperCase() })
+  saveButton.props.onPress()
+  expect(Alert.alert).toBeCalledTimes(1)
+  expect(Alert.alert.mock.calls).toMatchSnapshot()
 })
