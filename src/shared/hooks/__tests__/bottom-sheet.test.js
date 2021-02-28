037d4d01cb2169f1310a9dddaa2358532d249c05
Bruno Bodian <bacarybruno@gmail.con>
2021-02-28 16:46:34 +0100

    test: add bottom sheet tests

---
+++ src/shared/hooks/__tests__/bottom-sheet.test.js
@@ -0,0 +1,48 @@
+import { setupHook } from '@app/config/jest-setup'
+import { useActionSheet } from '@expo/react-native-action-sheet'
+import useBottomSheet from '../bottom-sheet'
+
+jest.mock('@expo/react-native-action-sheet', () => ({
+  useActionSheet: jest.fn().mockReturnValue({
+    showActionSheetWithOptions: jest.fn().mockImplementation((props, callback) => callback(0)),
+  }),
+}))
+
+const givenBottomSheet = () => {
+  const rendered = setupHook(() => useBottomSheet())
+  return { bottomSheet: rendered.result.current, rerender: rendered.rerender }
+}
+
+it('showManagePostsOptions', async () => {
+  const { bottomSheet } = givenBottomSheet()
+  await bottomSheet.showManagePostsOptions()
+  expect(useActionSheet().showActionSheetWithOptions.mock.calls[0]).toMatchSnapshot()
+})
+
+it('showSharePostOptions', async () => {
+  const { bottomSheet } = givenBottomSheet()
+  await bottomSheet.showSharePostOptions()
+  expect(useActionSheet().showActionSheetWithOptions.mock.calls[0]).toMatchSnapshot()
+})
+
+it('showAppearanceOptions', async () => {
+  const { bottomSheet } = givenBottomSheet()
+  await bottomSheet.showAppearanceOptions()
+  expect(useActionSheet().showActionSheetWithOptions.mock.calls[0]).toMatchSnapshot()
+})
+
+it('showAvatarOptions', async () => {
+  const { bottomSheet } = givenBottomSheet()
+  await bottomSheet.showAvatarOptions()
+  expect(useActionSheet().showActionSheetWithOptions.mock.calls[0]).toMatchSnapshot()
+})
+
+it('showFeedPostsOptions', async () => {
+  const { bottomSheet } = givenBottomSheet()
+  await bottomSheet.showFeedPostsOptions({
+    author: {
+      displayName: 'Bruno',
+    },
+  })
+  expect(useActionSheet().showActionSheetWithOptions.mock.calls[0]).toMatchSnapshot()
+})
