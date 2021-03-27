0131a422427bd9437823a3efde4cbae226ac8895
Bruno Bodian <bacarybruno@gmail.con>
2021-03-27 09:52:22 +0100

    style: add theme color in parsed typography component

--- src/shared/components/typography/parsed/styles.js
+++ src/shared/components/typography/parsed/styles.js
@@ -1,6 +1,9 @@
 import { StyleSheet } from 'react-native'
 
 export default (colors) => StyleSheet.create({
+  text: {
+    color: colors.textPrimary,
+  },
   link: {
     color: colors.link,
   },
